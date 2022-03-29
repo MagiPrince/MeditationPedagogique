b4f1d8355a583dbcee43f2f264f69a7da61bb682
David Nogueiras <david.nogueiras@outlook.com>
2022-03-29 19:09:09 +0200

    Starting project

---
+++ MeditationPedagogique/MeditationPedagogique_app/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    # Get all lectures in the DB
+    context = {
+        'test': 'CECI EST UN TEST'
+    }
+    return render(request, "index.html", context)
