196ebea919dc228f1875c58e2e4f5f1ab6d2f2e8
David Nogueiras <david.nogueiras@outlook.com>
2022-04-01 16:23:14 +0200

    Create registration form and modify admin user interface to enable roles (Student and Professor

--- MeditationPedagogique/MeditationPedagogique_app/models.py
+++ MeditationPedagogique/MeditationPedagogique_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    user = models.CharField(max_length=255, unique=True)
+    pass_w = models.CharField(max_length=50)
+    email = models.EmailField('Adresse mail', unique=True)
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=150, blank=False, null=False)
+
+    STUDENT = 1
+    PROFESSOR = 2
+
+    ROLE_CHOICES = (
+        (STUDENT, 'Student'),
+        (PROFESSOR, 'Professor'),
+    )
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, blank=True, null=True)
