8337c7e5e01113b429f2afa60667cc845da452d7
David Nogueiras <david.nogueiras@outlook.com>
2022-04-04 10:33:25 +0200

    Update user table to delete pass_w field and replace it with preexisting field password

--- MeditationPedagogique/MeditationPedagogique_app/models.py
+++ MeditationPedagogique/MeditationPedagogique_app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     user = models.CharField(max_length=255)
-    pass_w = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
     email = models.EmailField('Adresse mail', unique=True)
     first_name = models.CharField(max_length=30, blank=False, null=False)
     last_name = models.CharField(max_length=150, blank=False, null=False)
