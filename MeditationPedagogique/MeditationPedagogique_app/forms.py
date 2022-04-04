1065d1621351f64b3a4f7f743961e70d2e617abb
David Nogueiras <david.nogueiras@outlook.com>
2022-04-04 11:25:18 +0200

    Modify CustomUserForm to be more pretty and check some elements when creating user

--- MeditationPedagogique/MeditationPedagogique_app/forms.py
+++ MeditationPedagogique/MeditationPedagogique_app/forms.py
@@ -1,19 +1,53 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import User
+from django.contrib.auth import get_user_model
 
 
-class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+class CustomUserForm(UserCreationForm):
+    username = forms.CharField(label='username', min_length=4, max_length=150)
+    email = forms.EmailField(label='email', required=True)
+    first_name = forms.CharField(label='first name', max_length=50)
+    last_name = forms.CharField(label='last name', max_length=50)
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ("username", "email", "first_name", "last_name", "password1", "password2")
 
+
+    def username_clean(self):
+        username = self.cleaned_data['username'].lower()
+        new = get_user_model().objects.filter(username = username)
+        if new.count():
+            raise forms.ValidationError("User Already Exist")
+        return username
+    
+
+    def email_clean(self):
+        email = self.cleaned_data['email'].lower()
+        new = get_user_model().objects.filter(email=email)
+        if new.count():
+            raise forms.ValidationError("Email Already Exist")
+        return email
+
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+  
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Password don't match")
+        return password2
+
+
     def save(self, commit=True):
-        user = super(NewUserForm, self).save(commit=False)
+        user = super(CustomUserForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
-        user.role = User.STUDENT
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.role = get_user_model().STUDENT
         if commit:
             user.save()
         return user
--- MeditationPedagogique/MeditationPedagogique_app/views.py
+++ MeditationPedagogique/MeditationPedagogique_app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import  render, redirect
-from .forms import NewUserForm
+from .forms import CustomUserForm
 from django.contrib.auth import login
 from django.contrib import messages
 
@@ -15,13 +15,15 @@ def index(request):
 
 
 def register_request(request):
-	if request.method == 'POST':
-		form = NewUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			messages.success(request, 'Registration successful.' )
-			return redirect(index)
-		messages.error(request, 'Unsuccessful registration. Invalid information.')
-	form = NewUserForm()
-	return render(request=request, template_name='registration/inscription.html', context={'register_form': form})
+    context = {}
+    form = CustomUserForm(request.POST or None)
+    context['form'] = form
+    if request.method == 'POST':
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Registration successful.' )
+            return redirect(index)
+        #messages.error(request, 'Unsuccessful registration. Invalid information.')
+    form = CustomUserForm()
+    return render(request,'registration/inscription.html', context)
