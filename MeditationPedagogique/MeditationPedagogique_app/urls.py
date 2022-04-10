c2dd57d2cc4d3510602781d1c3768acc2479af0c
David Nogueiras <david.nogueiras@outlook.com>
2022-04-10 17:11:12 +0200

    some stuff

--- MeditationPedagogique/MeditationPedagogique_app/urls.py
+++ MeditationPedagogique/MeditationPedagogique_app/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth.views import PasswordChangeDoneView, PasswordChangeView
 urlpatterns=[
     path('', views.index, name='index'),
     path('inscription/', views.register_request, name='register'),
-    path('modifier_mdp/', PasswordChangeView.as_view(template_name='registration/change_password.html'), name='password_change'),
-    path('modifier_mdp_succes', PasswordChangeDoneView.as_view(template_name='registration/change_password_success.html'), name='password_change_done'),
+    path('change-password/', PasswordChangeView.as_view(template_name='registration/change_password.html'), name='password_change'),
+    path('change-password-success/', PasswordChangeDoneView.as_view(template_name='registration/change_password_success.html'), name='password_change_done'),
 ]
